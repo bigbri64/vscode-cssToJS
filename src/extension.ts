6e3a80165dd10ce2200e1f15dc5e6ca7a67c4c74
Rhys Tyers <rhystyers1@gmail.com>
2017-12-08 10:11:59 +0000

    handle exclation marks, handle multiple hyphens in css key, convert back from js object to css style

--- src/extension.ts
+++ src/extension.ts
@@ -21,17 +21,35 @@ class CSSToJS {
         if (editor) {
             let doc = editor.document;
             if (doc) {
+                // Get the selected text
                 let text = doc.getText(editor.selection);
-                console.log(text)
-                const reDeHyphenKey = /-(\w)(.*:)/g;
-                const reValue = /:\s*["']?([\w-(),\s.%#"']*)["']?;?/g;
-                text = text.replace(reDeHyphenKey, (m, p1, p2) => {
-                    return p1.toUpperCase() + p2;
-                });
-                text = text.replace(reValue, (m, p1) => {
-                    return ': "' + p1.replace(/([\"\'])/g,'\\'+'$1') + '",'
-                });
-                console.log(text);
+                // If the line ends with a comma it's probably a js object
+                const firstLine = text.split('\n')[0].trim();
+                if (firstLine[firstLine.length - 1] === ',') {
+                    const reReHyphenKey = /([A-Z])/g;
+                    const reValue = /:\s*["']?([\w-(),\s.%#"'!]*)["'],?[\n\r]/g;
+                    // Replace capital letters with hyphens and lowercase letters
+                    text = text.replace(reReHyphenKey, (m, p1) => {
+                        return "-" + p1.toLowerCase();
+                    });
+                    // Remove the quotes and remove string escaping
+                    text = text.replace(reValue, (m, p1) => {
+                        return ': ' + p1.replace(/([\\])/g,'') + ';\n'
+                    });
+                } else {
+                    const reDeHyphenKey = /-(\w)(.*:)/g;
+                    const reValue = /:\s*["']?([\w-(),\s.%#"'!]*)["']?;?[\n\r]/g;
+                    // Replace hyphens and the next letter with an uppercase letter
+                    while (text.search(reDeHyphenKey) !== -1) {
+                        text = text.replace(reDeHyphenKey, (m, p1, p2) => {
+                            return p1.toUpperCase() + p2;
+                        });
+                    }
+                    // Surround the value with quotes and escape any quotes in the value
+                    text = text.replace(reValue, (m, p1) => {
+                        return ': "' + p1.replace(/([\"\'])/g,'\\'+'$1') + '",\n'
+                    });
+                }
                 editor.edit((builder) => {builder.replace(editor.selection, text)});
             }
         }
